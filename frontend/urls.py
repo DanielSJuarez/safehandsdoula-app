7c734322313f9f3d9107985abc39228366ae2f64
Daniel Juarez <juarezdaniel@gmail.com>
2022-03-17 14:11:59 -0400

    updates frontend.urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,15 @@ from .views import IndexView
 app_name = 'frontend'
 
 urlpatterns = [
-    path('',IndexView.as_view(), name='index'), 
-    path('<path:resource>', IndexView.as_view())
+    path('home/',IndexView.as_view(), name='index'), 
+    path('login/',IndexView.as_view(), name='index'),
+    path('register/',IndexView.as_view(), name='index'),
+    path('create/',IndexView.as_view(), name='index'),
+    path('profile/',IndexView.as_view(), name='index'),
+    path('doula/',IndexView.as_view(), name='index'),
+    path('calendly/',IndexView.as_view(), name='index'),
+    path('reported/',IndexView.as_view(), name='index'),
+    path('why/',IndexView.as_view(), name='index'),
+    path('what/',IndexView.as_view(), name='index'),
+    path('how/',IndexView.as_view(), name='index'),
 ]
